0cefa704cab795b2be42da42d018d8da2bb44d83
yangnuoyu <46553752+yangnuoyu@users.noreply.github.com>
2020-07-20 08:48:01 -0700

    feat: add dashboard for shared dependency version checking (#730)

---
+++ .kokoro/nightly/dashboard.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Fail on any error.
+set -e
+# Display commands being run.
+set -x
+
+cd github/java-cloud-bom
+# M2_HOME is not used since Maven 3.5.0 https://maven.apache.org/docs/3.5.0/release-notes.html
+mvn -B clean install
+
+# Running target of dashboard submodule
+# https://stackoverflow.com/questions/3459928/running-a-specific-maven-plugin-goal-from-the-command-line-in-a-sub-module-of-a/26448447#26448447
+# https://stackoverflow.com/questions/11091311/maven-execjava-goal-on-a-multi-module-project
+cd dashboard
+
+# For all versions available in Maven Central and local repository
+mvn -B exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.cloudbomdashboard.DashboardMain" \
+  -Dexec.arguments="-a com.google.cloud:google-cloud-bom"
