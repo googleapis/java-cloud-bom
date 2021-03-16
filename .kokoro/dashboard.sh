c4f533cde300a3073b14500673b7497ed3e3fe21
stephwang <stephwang@google.com>
2021-03-16 16:15:00 -0400

    ci: add Github Actions workflow for dashboard
    To address PRs like #1747

---
+++ .kokoro/dashboard.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+## Get the directory of the build script
+scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+## cd to the parent directory, i.e. the root of the git repo
+cd ${scriptDir}/..
+
+## Move into the dashboard directory
+cd dashboard/
+
+echo -e "\n******************** BUILDING THE DASHBOARD ********************"
+
+mvn --fail-at-end clean install
