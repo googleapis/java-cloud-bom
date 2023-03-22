8136e6231c4483decdf30da7abb7637dae27c4d6
ddixit14 <deepankardixit@google.com>
2023-03-22 14:46:05 -0400

    chore: Cloud Java Usage Dashboard

---
+++ .kokoro/nightly/create-versions-csv.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#input -> text file that lists versionless coordinates
+
+#example ->
+#com.google.cloud:google-cloud-storage
+#com.google.cloud:google-cloud-vision
+
+#Output:
+#cloud_java_client_library_release_dates.csv that holds the data defined below. It has artifact_id,service_name,version, and release_date columns.
+
+cat .kokoro/nightly/libraries.txt | while read line; do
+
+  group_id=${line%:*}
+  artifact_id=${line#*:}
+  new_group_id="${group_id//.//}"
+  service_name=${artifact_id#*-cloud-}
+
+  if [[ "${artifact_id}" == *storage* ]]; then
+    service_name=bigstore
+  fi
+
+  URL=https://repo1.maven.org/maven2/$new_group_id/$artifact_id
+
+  .kokoro/nightly/fetch-library-data.sh $URL $artifact_id $service_name
+
+done
+
+sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > ./cloud_java_client_library_release_dates.csv
+sed -i '1s/^/version,release_date,artifact_id,service_name\n/' ./cloud_java_client_library_release_dates.csv
+
+rm -f cloud_java_client_library_release_dates_tsv.txt
---
+++ libraries-release-data/src/main/java/GenerateLibrariesList.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2018 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.cloud.dashboard;
+
+import com.google.cloud.tools.opensource.dependencies.Bom;
+import com.google.cloud.tools.opensource.dependencies.MavenRepositoryException;
+import com.google.common.collect.ImmutableList;
+import org.eclipse.aether.artifact.Artifact;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateLibrariesList {
+
+    public static void main (String [] argument) throws MavenRepositoryException {
+        System.out.println("running code");
+
+        Path bomPath = Paths.get("..", "libraries-bom", "pom.xml").toAbsolutePath();
+        Bom bom = Bom.readBom(bomPath);
+        List<Artifact> artifacts = bom.getManagedDependencies();
+        List<String> artifactList = new ArrayList<>();
+
+        for (Artifact art : artifacts) {
+            artifactList.add(art.getGroupId()+":"+art.getArtifactId());
+        }
+        System.out.println(artifactList);
+    }
+}
+
+
+
+
+
