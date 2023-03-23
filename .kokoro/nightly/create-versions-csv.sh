77aacfe8826396c733bb0b72ca1b5ba1cf87920d
Deepankar Dixit <deepankardixit@google.com>
2023-03-23 18:55:26 +0000

    chore: updating files

--- .kokoro/nightly/create-versions-csv.sh
+++ .kokoro/nightly/create-versions-csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #input -> text file that lists versionless coordinates
-
+#
 #example ->
 #com.google.cloud:google-cloud-storage
 #com.google.cloud:google-cloud-vision
@@ -8,7 +8,32 @@
 #Output:
 #cloud_java_client_library_release_dates.csv that holds the data defined below. It has artifact_id,service_name,version, and release_date columns.
 
-cat .kokoro/nightly/libraries.txt | while read line; do
+# Fail on any error.
+set -e
+# Display commands being run.
+#set -x
+
+#cd libraries-release-data
+## M2_HOME is not used since Maven 3.5.0 https://maven.apache.org/docs/3.5.0/release-notes.html
+#mvn -B clean install
+#
+#list=$(mvn -B exec:java -Dexec.mainClass="com.google.cloud.dashboard.GenerateLibrariesList")
+#
+#
+#final_list=$(echo $list | grep -o 'com.google.cloud[^,]*' | tr '\n' ',' | sed 's/,$//')
+#
+#
+#echo ${final_list} | tr ',' '\n' > unfiltered-libraries.txt
+##grep -v "libraries-release-data" libraries.txt
+#sed -i '/libraries-release-data/d' unfiltered-libraries.txt
+#sort unfiltered-libraries.txt | uniq > libraries.txt
+#rm -f unfiltered-libraries.txt
+
+
+##sed -r 's/.*-(.*)/\1/' libraries.txt >> services.txt
+#sed 's/com.google.cloud:google-cloud-//' libraries.txt > output.txt
+
+cat libraries-release-data/libraries.txt | while read line; do
 
   group_id=${line%:*}
   artifact_id=${line#*:}
@@ -28,4 +53,8 @@ done
 sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > ./cloud_java_client_library_release_dates.csv
 sed -i '1s/^/version,release_date,artifact_id,service_name\n/' ./cloud_java_client_library_release_dates.csv
 
+bq load --autodetect --source_format=CSV cloud-java-metrics.client_library_versions.cloud_java_client_library_release_dates cloud_java_client_library_release_dates.csv
+
+
 rm -f cloud_java_client_library_release_dates_tsv.txt
+rm -f cloud_java_client_library_release_dates.csv
