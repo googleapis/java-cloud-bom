aa5038cfa30cb88d01113ac69ee920cc5614e809
Deepankar Dixit <90280028+ddixit14@users.noreply.github.com>
2023-03-28 19:16:23 +0000

    chore: fixing library release date retrieval script (#5892)
    chore: fixing script

--- .kokoro/nightly/create-versions-csv.sh
+++ .kokoro/nightly/create-versions-csv.sh
@@ -9,12 +9,15 @@
 # Fail on any error.
 set -e
 # Display commands being run.
-#set -x
+set -x
 
-cd github/java-cloud-bom/libraries-release-data
+cd github/java-cloud-bom
 
-# M2_HOME is not used since Maven 3.5.0 https://maven.apache.org/docs/3.5.0/release-notes.html
 mvn -B clean install
+
+cd libraries-release-data
+
+mvn compile
 
 list=$(mvn -B exec:java -Dexec.mainClass="com.google.cloud.dashboard.GenerateLibrariesList")
 
@@ -28,10 +31,7 @@ sort unfiltered-libraries.txt | uniq > libraries.txt
 rm -f unfiltered-libraries.txt
 
 
-cd github/java-cloud-bom
-
-
-cat libraries-release-data/libraries.txt | while read line; do
+cat libraries.txt | while read line; do
 
   group_id=${line%:*}
   artifact_id=${line#*:}
@@ -44,17 +44,17 @@ cat libraries-release-data/libraries.txt | while read line; do
 
   URL=https://repo1.maven.org/maven2/$new_group_id/$artifact_id
 
-  .kokoro/nightly/fetch-library-data.sh $URL $artifact_id $service_name
+  ../.kokoro/nightly/fetch-library-data.sh $URL $artifact_id $service_name
 
 done
 
-rm -f libraries-release-data/libraries.txt
+rm -f libraries.txt
 
-sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > ./cloud_java_client_library_release_dates.csv
-sed -i '1s/^/version,release_date,artifact_id,service_name\n/' ./cloud_java_client_library_release_dates.csv
+sed 's/ \+/,/g' cloud_java_client_library_release_dates_tsv.txt > cloud_java_client_library_release_dates.csv
+sed -i '1s/^/version,release_date,artifact_id,service_name\n/' cloud_java_client_library_release_dates.csv
 
 echo "Inserting client_library_versions.cloud_java_client_library_release_dates. First 10 lines:"
-head  ./cloud_java_client_library_release_dates.csv
+head  cloud_java_client_library_release_dates.csv
 echo "===================="
 
 bq load --autodetect --project_id=cloud-java-metrics --source_format=CSV \
