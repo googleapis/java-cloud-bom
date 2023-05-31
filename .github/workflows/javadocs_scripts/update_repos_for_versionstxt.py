0bdfde185524d032fa7d21a5fc4f0a5ae782fff2
Alice Li <alicejli@google.com>
2023-05-31 15:43:55 -0400

    clean up workflows and add script to update repo versions

---
+++ .github/workflows/javadocs_scripts/update_repos_for_versionstxt.py
@@ -0,0 +1,26 @@
+import yaml
+import fileinput
+
+# This script updates the versions of the sdk-platform-java and google-cloud-java for checkout
+def update_repos_for_versionstxt(file_path, yaml_file):
+  with open(yaml_file, 'r') as yaml_data:
+    version_data = yaml.safe_load(yaml_data)
+
+  for line in fileinput.input(file_path, inplace=True):
+    parts = line.strip().split(' ')
+    if len(parts) == 4:
+      repo, directory, tag, artifact_id = parts
+      if artifact_id in version_data:
+        version = version_data[artifact_id]
+        updated_line = f"{repo} {directory} {version} {artifact_id}\n"
+        print(updated_line, end='')
+      else:
+        print(line, end='')
+    else:
+      print(line, end='')
+
+# Example usage
+file_path = '.github/workflows/javadocs_scripts/repos_for_versionstxt.txt'
+yaml_file = './site/data/variables.yaml'
+
+update_repos_for_versionstxt(file_path, yaml_file)
