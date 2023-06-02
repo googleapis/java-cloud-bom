c1d78f1c0741a1f665bf3d411c540b6bd0b03acc
Alice Li <alicejli@google.com>
2023-06-02 09:49:04 -0400

    add handwritten library javadoc generation workflow

---
+++ .github/workflows/javadocs_scripts/update_repo_versions.py
@@ -0,0 +1,29 @@
+import yaml
+import fileinput
+
+# This script updates the versions of the sdk-platform-java, google-cloud-java, and handwritten libraries for checkout
+def update_repo_versions(file_paths, yaml_file):
+  with open(yaml_file, 'r') as yaml_data:
+    version_data = yaml.safe_load(yaml_data)
+
+  for file_path in file_paths:
+    for line in fileinput.input(file_path, inplace=True):
+      parts = line.strip().split(' ')
+      if len(parts) == 4:
+        repo, directory, tag, artifact_id = parts
+        if artifact_id in version_data:
+          version = version_data[artifact_id]
+          # exception for sdk-platform-java to use google-cloud-shared-dependencies tag
+          if artifact_id == "first-party-dependencies":
+            version = 'google-cloud-shared-dependencies/' + version
+          updated_line = f"{repo} {directory} {version} {artifact_id}\n"
+          print(updated_line, end='')
+        else:
+          print(line, end='')
+      else:
+        print(line, end='')
+
+file_paths = ['.github/workflows/javadocs_scripts/repos_for_versionstxt.txt', '.github/workflows/javadocs_scripts/handwritten_libraries_javadocs_modules.txt']
+yaml_file = './site/data/variables.yaml'
+
+update_repo_versions(file_paths, yaml_file)
