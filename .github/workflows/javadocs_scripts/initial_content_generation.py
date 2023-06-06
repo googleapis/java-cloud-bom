d971d8fc59f3e119d7d96ccac07462ff0cd9328e
Alice Li <alicejli@google.com>
2023-06-06 17:03:38 -0400

    update alphabetical order and add content to main _index.md page

--- .github/workflows/javadocs_scripts/initial_content_generation.py
+++ .github/workflows/javadocs_scripts/initial_content_generation.py
@@ -7,27 +7,38 @@ from jinja2 import Environment, FileSystemLoader
 with open('site/data/variables.yaml', 'r') as f:
   data = yaml.safe_load(f)
 
-# Prepare the markdown template
+# Template for modules that have a Product page
 markdown_template = """
 ### {{key}}
 - {% raw %}{{< variable {% endraw %}"{{key}}" >{% raw %}}}{% endraw %}([Javadocs]({% raw %}{{< variable "github-repo" >{% endraw %}{% raw %}}}{% endraw %}/{{key}}/), [Google Cloud Java Library Reference]({% raw %}{{< variableLibraryReferenceLinks{% endraw %} "{{key}}" >{% raw %}}}{% endraw %}))
 - [Google Cloud Product Reference]({% raw %}{{< variableProductReferenceLinks {% endraw %}"{{key}}" >{% raw %}}}{% endraw %})
 """
 
-# Set up Jinja environment
+# Template for modules that do not have a Product page
+markdown_template_no_product = """
+### {{key}}
+- {% raw %}{{< variable {% endraw %}"{{key}}" >{% raw %}}}{% endraw %}([Javadocs]({% raw %}{{< variable "github-repo" >{% endraw %}{% raw %}}}{% endraw %}/{{key}}/), [Google Cloud Java Library Reference]({% raw %}{{< variableLibraryReferenceLinks{% endraw %} "{{key}}" >{% raw %}}}{% endraw %}))
+"""
+
 env = Environment(loader=FileSystemLoader('.'))
-template = env.from_string(markdown_template)
+template_with_product = env.from_string(markdown_template)
+template_without_product = env.from_string(markdown_template_no_product)
 
 # Initialize the final output as an empty string
 output = ''
 
-# List of keys to ignore
-keys_to_ignore = ['libraries-bom', 'gapic-libraries-bom','github-repo','gax-httpjson', 'google-cloud-shared-dependencies', 'first-party-dependencies']
+# List of modules to ignore
+modules_to_ignore = ['libraries-bom', 'gapic-libraries-bom','github-repo','gax-httpjson', 'google-cloud-shared-dependencies', 'first-party-dependencies']
 
-# For each key-value pair in the YAML file, render the template and add it to the output
-for key, value in data.items():
-  if key not in keys_to_ignore:
-    output += template.render(key=key, value=value)
+# List of modules that do not have a Product page
+modules_without_product = ['gax', 'google-iam-policy', 'google-cloud-core', 'api-common']
+
+# For each key-value pair in the sorted keys of the YAML file, render the template and add it to the output
+for key in sorted(data.keys()):
+  if key not in modules_to_ignore and key in modules_without_product:
+    output += template_without_product.render(key=key, value=data[key])
+  if key not in modules_to_ignore and key not in modules_without_product:
+    output += template_with_product.render(key=key, value=data[key])
 
 # Write to markdown file
 with open('output.md', 'w') as f:
