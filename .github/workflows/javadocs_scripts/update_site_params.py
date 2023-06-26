c8f0e4c7566179718a1c64f252edcd9ad2e63641
Alice Li <alicejli@google.com>
2023-06-26 11:50:21 -0400

    feat: Github Pages for Javadocs

---
+++ .github/workflows/javadocs_scripts/update_site_params.py
@@ -0,0 +1,23 @@
+# Run after parse_pom.py to update the config.toml file with the libraries-bom version
+
+import yaml
+import toml
+
+def main():
+  # Load the variables.yaml file
+  with open('./site/data/variables.yaml') as file:
+    variables = yaml.full_load(file)
+
+  # Load the config.toml file
+  with open('./site/config.toml', 'r') as file:
+    config = toml.load(file)
+
+  # Update `libraries_bom_version`
+  config['params']['libraries_bom_version'] = variables['libraries-bom']
+
+# Write the updated data back to the config.toml file
+  with open('./site/config.toml', 'w') as file:
+    toml.dump(config, file)
+
+if __name__ == "__main__":
+  main()
