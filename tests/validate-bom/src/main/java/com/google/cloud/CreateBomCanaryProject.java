8f98f1482a60eb46f70b66d9bb6437361df63e4f
mpeddada1 <mpeddada@google.com>
2023-09-19 20:05:37 +0000

    restore changes

--- tests/validate-bom/src/main/java/com/google/cloud/CreateBomCanaryProject.java
+++ tests/validate-bom/src/main/java/com/google/cloud/CreateBomCanaryProject.java
@@ -115,7 +115,9 @@ public class CreateBomCanaryProject {
       // Client Libraries for Java. Checking for availability of these unused artifacts on Maven
       // Central has caused BOM validation check to fail in the past. See
       // https://github.com/googleapis/sdk-platform-java/pull/1989#issuecomment-1724039670
-      if ("grpc-android".equals(managedDependency.getArtifactId())) {
+      if ("grpc-android".equals(managedDependency.getArtifactId())
+          || "grpc-binder".equals(managedDependency.getArtifactId())
+          || "grpc-cronet".equals(managedDependency.getArtifactId())) {
         continue;
       }
 
