e3ee446c59dad5d31fe87b4f8f95064541335e7f
Tomo Suzuki <suztomo@google.com>
2025-03-14 13:22:40 -0400

    hermetic build status

--- tests/release-cycle-readiness-head/check_readiness.sh
+++ tests/release-cycle-readiness-head/check_readiness.sh
@@ -8,7 +8,9 @@ WORK_DIR=/tmp/release-readiness
 
 cd "${WORK_DIR}/sdk-platform-java"
 expected_shared_deps_version=$(mvn -pl java-shared-dependencies help:evaluate -Dexpression=project.version -q -DforceStdout)
+expected_generator_version=$(mvn -pl gapic-generator-java help:evaluate -Dexpression=project.version -q -DforceStdout)
 echo "Expected google-cloud-shared-dependencies BOM version: ${expected_shared_deps_version}"
+echo "Expected GAPIC generator Java version: ${expected_generator_version}"
 
 repositories=$(find "${WORK_DIR}" -mindepth 1 -maxdepth 1 -type d -not -name "sdk-platform-java")
 for repo_folder in $repositories; do
@@ -23,10 +25,24 @@ for repo_folder in $repositories; do
     project=.
   fi
   actual_shared_deps_version=$(mvn -pl ${project} help:evaluate -Dexpression=google-cloud-shared-dependencies.version -q -DforceStdout)
-  if [ "${expected_shared_deps_version}" != "${actual_shared_deps_version}" ]; then
-    echo "${repo}: Not yet... expected: ${expected_shared_deps_version}, actual: ${actual_shared_deps_version}"
+  if [[ "${expected_shared_deps_version}" != "${actual_shared_deps_version}" ]]; then
+    shared_deps_status="Not yet(${actual_shared_deps_version})"
   else
-    echo "${repo}: OK (${actual_shared_deps_version})"
+    shared_deps_status="OK"
   fi
+
+  if [ -r "generation_config.yaml" ]; then
+    actual_generator_version=$(perl -nle 'print $1 if m/gapic_generator_version:\s*(.+)/' generation_config.yaml)
+    if [[ "${expected_generator_version}" != "${actual_generator_version}" ]]; then
+        generator_status="Not yet(${actual_generator_version})"
+    else
+        generator_status="OK"
+    fi
+  else
+    generator_status="N/A"
+  fi
+  echo "${repo} | ${shared_deps_status} | ${generator_status}"
 done
+
+
 
